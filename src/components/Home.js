467dcf24c1a2062247690986d1dfd91baa602248
Abhishek Arora <Cadenza@Abhisheks-MacBook-Air.local>
2018-04-28 21:32:02 +0530

    About me updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,19 @@ import ContentWrapper from './ContentWrapper';
 const Home = () => (
   <div>
     <ContentWrapper
-      title='About Me'
-    >
-      <p>Home</p>
+      title='About Me'>
+      <p>
+        Hi, I am <b>Abhishek Arora</b> currently working at Gamooga. Former employee at Cognizant and a consultant at JP Morgan Chase, my desire to work as a software developer has been sparked both by the variety and the quality of experiences it offers.
+      </p>
+      <p>
+        In this data-driven era, where data being generated is much more than the CO2 in our environment, organizations understand the need to capture all the data that streams into their businesses and apply analytics on that to get significant value from it.
+      </p>
+      <p>
+        Hence I chose Big Data, to help organizations harness their data and use it to identify new opportunities. Reducing the current costs and helping them make better and faster decisions.
+      </p>
+      <p>
+        Experienced Project Associate with a demonstrated history of working in the information technology and services industry. Skilled in Big Data Technologies like Hadoop, Hive, Java, Sqoop, Kafka, Data Warehousing, and Solr. Strong operations professional with a Bachelor's Degree focused in Engineering Physics/Applied Physics from IIT Delhi.
+      </p>
     </ContentWrapper>
   </div>
 );
