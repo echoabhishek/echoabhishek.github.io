e853c676146579df2120ac890a75c47039a9905e
Abhishek Arora <Cadenza@Abhisheks-MacBook-Air.local>
2018-04-28 21:31:52 +0530

    Route components linked

--- src/app/Content.js
+++ src/app/Content.js
@@ -4,6 +4,11 @@ import Navbar from './Navbar';
 import Home from '../components/Home';
 import About from '../components/About';
 import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Milestone from '../components/Milestone';
+import Resume from '../components/Resume';
+import Contact from '../components/Contact';
+import Blog from '../components/Blog';
 
 class Content extends React.Component {
   render() {
@@ -17,6 +22,11 @@ class Content extends React.Component {
             <Route path='/home' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/skills' component={Skills} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/milestone' component={Milestone} />
+            <Route path='/resume' component={Resume} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/blog' component={Blog} />
           </div>
         </Router>
       </div>
--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -21,6 +21,31 @@ const Navbar = (props) => {
         to='/skills'>
         <i class='material-icons md-dark'>build</i>
       </NavButton>
+      <NavButton
+        title='PROJECTS'
+        to='/projects'>
+        <i class='material-icons md-dark'>gavel</i>
+      </NavButton>
+      <NavButton
+        title='MILESTONE'
+        to='/milestone'>
+        <i class='material-icons md-dark'>donut_large</i>
+      </NavButton>
+      <NavButton
+        title='RESUME'
+        to='/resume'>
+        <i class='material-icons md-dark'>insert_drive_file</i>
+      </NavButton>
+      <NavButton
+        title='CONTACT'
+        to='/contact'>
+        <i class='material-icons md-dark'>chat_bubble</i>
+      </NavButton>
+      <NavButton
+        title='BLOG'
+        to='/blog'>
+        <i class='material-icons md-dark'>face</i>
+      </NavButton>
     </ul>
   );
 }
