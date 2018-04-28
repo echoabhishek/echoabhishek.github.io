9448055bcb54a41b4ead9aa574da88a15f44cc16
Abhishek Arora <Cadenza@Abhisheks-MacBook-Air.local>
2018-04-28 20:48:32 +0530

    SVG icons removed and material icons added

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import NavButton from './NavButton';
-import HomeIcon from '../static/icons/HomeIcon';
-import AboutIcon from '../static/icons/AboutIcon';
-import SkillsIcon from '../static/icons/SkillsIcon';
 import withStyles from 'material-ui/styles/withStyles';
 
-const styles = {
-    navbar: {
-        textAlign: 'center',
-        margin: '80px 0px 20px'
-    }
+const Navbar = (props) => {
+  const { classes } = props;
+  return (
+    <ul className={classes.navBar}>
+      <NavButton
+        title='HOME'
+        to='/home'>
+        <i class='material-icons md-dark'>home</i>
+      </NavButton>
+      <NavButton
+        title='ABOUT'
+        to='/about'>
+        <i class='material-icons md-dark'>person</i>
+      </NavButton>
+      <NavButton
+        title='SKILLS'
+        to='/skills'>
+        <i class='material-icons md-dark'>build</i>
+      </NavButton>
+    </ul>
+  );
 }
 
-const Navbar = (props) => {
-    const { classes } = props;
-        return (
-        <ul className={classes.navbar}>
-            <NavButton
-                title='HOME'
-                to='/home'>
-                <HomeIcon />
-            </NavButton>
-            <NavButton
-                title='ABOUT'
-                to='/about'>
-                <AboutIcon />
-            </NavButton>
-            <NavButton
-                title='SKILLS'
-                to='/skills'>
-                <SkillsIcon />
-            </NavButton>
-        </ul>
-    );
+const styles = {
+  navBar: {
+    textAlign: 'center',
+    margin: '80px 0px 20px'
+  }
 }
 
 export default withStyles(styles)(Navbar);
