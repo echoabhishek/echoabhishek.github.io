dcd122d1a0cac277f11e856446b16f9fd3d549ec
Abhishek Arora <Cadenza@Abhisheks-MacBook-Air.local>
2018-04-29 02:12:41 +0530

    Added resume to the resume route

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import ResumeFile from '../static/AbhishekArora-Resume.pdf'
 import ContentWrapper from './ContentWrapper';
 
 const Resume = () => (
   <div>
     <ContentWrapper
-      title='Resume'
-    >
-      <p>Resume</p>
+      title='Resume'>
+      <iframe
+        style={styles.iframe}
+        src={ResumeFile} />
     </ContentWrapper>
   </div>
 );
+
+const styles = {
+  iframe: {
+    width: '100%',
+    height: '700px',
+    zoom: 0.75,
+    fontSize: '150%',
+    padding: '0 0 0 0',
+    margin: '0 0 0 0',
+    marginTop: '30px'
+  }
+}
 
 export default Resume;
