7313f4897f883afe42c2b7a5725a73230dead61b
Abhishek Arora <hey@abhishek.id>
2023-04-30 01:44:57 +0530

    testing script

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,16 @@ const App = () => (
   <div>
     <Header />
     <Content />
+    <!-- START GAMOOGA BASE SCRIPT -->
+        <script type="text/javascript">
+            var _taq = {"id":"806dc637-2e30-4a36-8b50-334c1d99d415","events":[],"identify":[],"property":[], "handlers":[]};
+            (function() {
+                var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true; ta.id = "__ta";
+                ta.src = '//cdn-jp.gsecondscreen.com/static/ta.min.js';
+                var fs = document.getElementsByTagName('script')[0]; fs.parentNode.insertBefore(ta, fs);
+            })();
+        </script>
+    <!-- END GAMOOGA BASE SCRIPT -->
   </div>
 );
 
